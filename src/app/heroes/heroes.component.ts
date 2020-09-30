8ee72e5518caea2807510ea3dd54a3306f872221
Antonio Vera <veraortegaantonio@gmail.com>
2020-09-30 12:24:20 -0500

    Refactor heroes component to use heroes.service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interfaces/hero';
-import { HEROES } from "../mock-heroes";
+import { HeroService } from "../services/hero.service";
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes:Array<Hero>;
   hero:Hero = {
     id:1,
     name:'Windstorm'
   };
   
-  constructor() { }
+  constructor(private heroService:HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
+
   selectedHero: Hero;
   onSelect(hero: Hero):void{
     this.selectedHero = hero;
   }
 
+  getHeroes():void{
+    this.heroes = this.heroService.getHeroes();
+  }
 }
