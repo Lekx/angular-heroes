ad68520d6c2a7a3b96969f9683d32d37d5936b18
Antonio Vera <veraortegaantonio@gmail.com>
2020-09-30 13:04:48 -0500

    refactor hero.servise to use observable

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from "rxjs";
 import { Hero } from "../interfaces/hero";
 import { HEROES } from "../mock-heroes";
 @Injectable({
@@ -8,7 +9,7 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[]{
-    return HEROES;
+  getHeroes(): Observable<Hero[]>{
+    return of(HEROES);
   }
 }
